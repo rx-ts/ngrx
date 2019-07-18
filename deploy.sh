9c9a12586bbcfb3b67311a71ff57d1c3b871d608
JounQin <admin@1stg.me>
2019-07-18 23:49:56 +0800

    chore: toggle to release brach correctly on CI

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@ set -e
 git remote set-url origin https://user:$GH_TOKEN@github.com/rx-ts/ngrx.git
 npm set //registry.npmjs.org/:_authToken $NPM_TOKEN
 
+git fetch origin master:master
+git checkout release
+git rebase master
 yarn run standard-version -a
 git push --follow-tags origin release:master
 
