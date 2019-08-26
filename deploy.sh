7fb33ce0bacfe2e2b3e33a982fcd5e07fccd8bb1
JounQin <admin@1stg.me>
2019-08-26 12:16:33 +0800

    chore: lock root version to 0.0.0

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ git remote set-url origin "https://user:$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.g
 npm set //registry.npmjs.org/:_authToken "$NPM_TOKEN"
 
 git fetch origin "$TRAVIS_BRANCH":"$TRAVIS_BRANCH"
-git checkout master
+git checkout "$TRAVIS_BRANCH"
 
 PKG_VERSION=$(jq -r '.version' src/package.json)
 
@@ -14,8 +14,10 @@ git fetch origin v"$PKG_VERSION" || {
   yarn global add standard-version
   standard-version -a --release-as "$PKG_VERSION"
   tmp=$(mktemp)
-  jq '.version = "0.0.0"' package.json> "$tmp" && \mv "$tmp" package.json
+  jq '.version = "0.0.0"' package.json > "$tmp"
+  \mv -f "$tmp" package.json
   git commit --amend --no-edit
-  git push --follow-tags origin master
+  git tag -a v"$PKG_VERSION" -m "chore(release): $PKG_VERSION"
+  git push --follow-tags origin "$TRAVIS_BRANCH"
   npm publish dist --access=public
 }
